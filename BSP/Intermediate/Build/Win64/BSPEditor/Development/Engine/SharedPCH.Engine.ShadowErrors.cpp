ca64d649d5d480c653805d6d0c9ed6939f7c652b
Brendan <98858863+BrendanCopot@users.noreply.github.com>
2023-12-09 09:01:05 +1100

    Add Project Files

---
+++ BSP/Intermediate/Build/Win64/BSPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.35.32215
+#include "C:/Users/Brendan/Documents/Programming/Unreal/BSP/Intermediate/Build/Win64/BSPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ BSP/Source/BSP/BSPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BSPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BSP_API ABSPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
