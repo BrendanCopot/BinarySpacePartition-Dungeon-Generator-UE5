13dfe8fc55f0b3eeaba71634245f1d91bd93bdbe
Brendan <98858863+BrendanCopot@users.noreply.github.com>
2024-01-15 10:20:01 +1100

    Create AssetManager
    Created AssetManager script to handle loading/unloading assets

---
+++ BSPDungeon/Source/BSPDungeon/BSPDungeonAssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BSPDungeonAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BSPDUNGEON_API UBSPDungeonAssetManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
